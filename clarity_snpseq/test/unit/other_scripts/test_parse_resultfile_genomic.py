92f688aaaae4485e99af4aee0f4e06ea5ab15d55
GitEdvard <edvard.englund@medsci.uu.se>
2018-05-02 11:00:01 +0200

    Add tests to parse resultfile genomic
    Copy example tapestation xml file to test environment

---
+++ clarity_snpseq/test/unit/other_scripts/test_parse_resultfile_genomic.py
@@ -0,0 +1,65 @@
+import unittest
+from unittest import skip
+from jinja2 import Environment, PackageLoader
+from clarity_snpseq.test.utility.higher_level_builders import ReadResultFileBuilder
+from clarity_ext_scripts.tapestation.parse_result_file_genomic import Extension as ParseResultfileGenomic
+from clarity_snpseq.test.utility.context_monkey_patching import ResultFilePatcher
+
+
+class TestParseResultfileGenomic(unittest.TestCase):
+    """
+    This script is sorted under tapestation scripts
+    """
+    def setUp(self):
+        builder = ReadResultFileBuilder()
+        builder.with_analyte_udf('din', None)
+        builder.with_analyte_udf('TS Total Conc. ng/uL', None)
+        builder.with_analyte_udf('TS Range Conc. ng/uL', None)
+        builder.with_analyte_udf('Peak 1 mw', None)
+        builder.with_analyte_udf('TS Length (bp)', None)
+        builder.with_analyte_udf('TS Observation', None)
+        builder.with_analyte_udf('Dil. Calc. Source Vol.', None)
+        builder.with_mocked_local_shared_file('Result XML File (required)')
+        self.builder = builder
+
+    def _create_pair(self, target_artifact_id, artifact_name=None):
+        return self.builder.create_pair(target_artifact_id, artifact_name)
+
+    def _init_builder(self, contents_as_list, container, pair):
+        self.builder.create(
+            ParseResultfileGenomic, contents_as_list, container, pair)
+
+    def peaks(self, well):
+        if well.artifact.name == "Ladder":
+            return [{"area": 1.0}] * 14
+        else:
+            return [{"area": 1.0}, {"area": 4.69}, {"area": 0.03}]
+
+    def _render_xml(self):
+        self.builder.extension.peaks = self.peaks
+        env = Environment(loader=PackageLoader('clarity_snpseq', 'test/unit/other_scripts'))
+        template = env.get_template('example_tapestation_file.xml.j2')
+        return template.render(ext=self.builder.extension)
+
+    def test_render(self):
+        container, pair = self._create_pair('92-998')
+        self._init_builder([], container, pair)
+        self._render_xml()
+
+    def first_test(self):
+        # Arrange
+        container, pair = self._create_pair(target_artifact_id='92-998')
+        self._init_builder([], container, pair)
+        contents = self._render_xml()
+        self.builder.context_builder.with_mocked_local_shared_file(
+            'Result XML File (required)', contents)
+        monkey = ResultFilePatcher()
+        monkey.cache['92-998'] = pair.output_artifact
+        self.builder.extension.context.output_result_file_by_id = monkey.output_result_file_by_id
+
+        # Act
+        self.builder.extension.execute()
+
+        # Assert
+        #self.assertEqual(5.48, pair.output_artifact.udf_gqn)
+
--- clarity_snpseq/test/utility/context_monkey_patching.py
+++ clarity_snpseq/test/utility/context_monkey_patching.py
@@ -1,5 +1,10 @@
+from contextlib import contextmanager
 from StringIO import StringIO
+from io import BytesIO
+from mock import MagicMock
 from clarity_ext.service.file_service import Csv
+from clarity_ext.service.file_service import FileService
+from clarity_snpseq.test.utility.fake_collaborators import FakeOsService
 
 
 class LocalSharedFilePatcher:
@@ -8,14 +13,29 @@ class LocalSharedFilePatcher:
     """
     def __init__(self):
         self.cache = dict()
+        self.file_service = FileService(None, None, False, FakeOsService())
 
     def local_shared_file(self, name, mode="r", is_xml=False, is_csv=False, file_name_contains=None):
+        @contextmanager
+        def fake_stream(io_obj):
+            yield io_obj
+
         if is_csv:
             content = self.cache[name]
             stream = StringIO(content)
             return Csv(stream)
         else:
-            raise NotImplementedError
+            content = self.cache[name]
+            bs = BytesIO(b'{}'.format(content.encode('utf-8')))
+            return self.file_service.parse_xml(bs)
+
+
+class ResultFilePatcher:
+    def __init__(self):
+        self.cache = dict()
+
+    def output_result_file_by_id(self, file_id):
+        return self.cache[file_id]
 
 
 class UseQcFlagPatcher:
